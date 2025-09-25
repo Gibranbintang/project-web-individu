eb53a5179215d170c2eb64db82b7f250f2d1805d
Gibranbintang <gibranbintang2004@gmail.com>
2025-09-25 20:26:32 +0700

    init

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>nama:Gibran Bintang Hakim</h1>
+    <h2>NIM:13242420006</h2>
+</body>
+</html>
