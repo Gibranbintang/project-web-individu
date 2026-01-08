763c2d9f43f100c9a6c3b8d5e9febad9695e4ca3
Gibranbintang <gibranbintang2004@gmail.com>
2026-01-08 14:17:26 +0700

    menambah fitur switch alert kemarin

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -1,21 +1,29 @@
 @extends('layouts.main')
 
 @section('content')
-  
-
-
-
-
-<body>
+<div class="container mt-4">
+    <h2>Data Mahasiswa</h2>
     
-
-    <div class="container mt-4">
-        <h2>Data Mahasiswa</h2>
-        
-        <a href="tambahmahasiswa">
-            <button type="button" class="btn btn-success mb-3">Tambah Data</button>
+    <div class="row mt-4">
+        <a href="/tambahmahasiswa">
+            <button type="button" class="btn btn-success mb-2">
+                Tambah Data
+            </button>
         </a>
-
+        
+        <!-- SweetAlert Success Message -->
+        @if ($message = Session::get('success'))
+            <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    Swal.fire({
+                        title: "Berhasil!",
+                        text: "{{ $message }}",
+                        icon: "success"
+                    });
+                });
+            </script>
+        @endif
+        
         <table class="table table-bordered">
             <thead class="thead-dark">
                 <tr>
@@ -28,22 +36,61 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach($mahasiswas as $index => $mhs)
-                <tr>
-                    <th scope="row">{{ $index + 1 }}</th>
-                    <td>{{ $mhs->name }}</td>
-                    <td>{{ $mhs->nim }}</td>
-                    <td>{{ $mhs->prodi }}</td>
-                    <td>{{ $mhs->email }}</td>
-                    <td>
-                        <a href="tampildata/{{ $mhs ->id }}"class="btn btn-primary">Edit</a>
-                        <a href="deletedata/{{ $mhs ->id }}"class="btn btn-danger" onclick="return confirm('yakin dihapus?')">Hapus</a>
-                        
-                    </td>
-                </tr>
+                <?php $i = 1 ?>
+                @foreach ($mahasiswas as $mahasiswa)
+                    <tr>
+                        <th scope="row">{{ $i }}</th>
+                        <td>{{ $mahasiswa->name }}</td>
+                        <td>{{ $mahasiswa->nim }}</td>
+                        <td>{{ $mahasiswa->prodi }}</td>
+                        <td>{{ $mahasiswa->email }}</td>
+                        <td>
+                            <a href="/tampildata/{{ $mahasiswa->id }}" class="btn btn-sm btn-warning">
+                                Edit
+                            </a>
+                            <a href="#" 
+                               class="btn btn-sm btn-danger delete"
+                               data-id="{{ $mahasiswa->id }}"
+                               data-nama="{{ $mahasiswa->name }}">
+                                Hapus
+                            </a>
+                        </td>
+                        <?php $i++ ?>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
-</body>
+</div>
+
+<!-- SweetAlert2 CDN -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- jQuery CDN -->
+<script src="https://code.jquery.com/jquery-3.7.1.js" 
+        integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" 
+        crossorigin="anonymous"></script>
+
+<!-- Delete Confirmation Script -->
+<script>
+    $('.delete').click(function () {
+        let id = $(this).attr('data-id');
+        let nama = $(this).attr('data-nama');
+        
+        Swal.fire({
+            title: "Are you sure?",
+            text: "Data " + nama + " akan terhapus.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "Cancel"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location = "/deletedata/" + id;
+            }
+        });
+    });
+</script>
 @endsection
